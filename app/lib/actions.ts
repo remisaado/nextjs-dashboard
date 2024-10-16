30589207d46fafdb3832eaf7b48821d0eb0ff6ff
remisaado <remi_saado@hotmail.com>
2024-10-16 09:48:06 +0200

    Fixes errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -57,6 +57,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
             VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
     } catch (error) {
+        console.log(error);
         return { message: 'Database Error: Failed to Create Invoice.' };
     }
 
@@ -88,6 +89,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
             WHERE id = ${id}
         `;
     } catch (error) {
+        console.log(error);
         return { message: 'Database Error: Failed to Update Invoice.' };
     }
 
@@ -101,6 +103,7 @@ export async function deleteInvoice(id: string) {
         revalidatePath('/dashboard/invoices');
         return { message: 'Deleted Invoice.' };
     } catch (error) {
+        console.log(error);
         return { message: 'Database Error: Failed to Delete Invoice.' };
     }
     
